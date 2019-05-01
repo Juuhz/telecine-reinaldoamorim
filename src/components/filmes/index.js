42aa4b5536a52121895718ae44e9a5d07134aa34
Reinaldo <juuhz.easy@gmail.com>
2019-05-01 13:21:45 -0300

    [Update] - Header mobile finalizado

---
+++ src/components/filmes/index.js
@@ -0,0 +1,23 @@
+// Import Ultils
+import { Container } from '../../utils/class';
+
+// Import API
+//import HeaderProps from '../../api/header';
+
+// Import Style
+import {
+    Section
+} from './styled';
+
+const Filmes = () => {
+    //const { title, text, buttonCTA } = HeaderProps;
+    return(
+        <Section id="filmes">
+            <Container>
+                
+            </Container>
+        </Section>
+    );
+}
+
+export default Filmes;
---
+++ src/components/filmes/styled.js
@@ -0,0 +1,15 @@
+// Import Styled
+import styled from 'styled-components';
+
+// Import Utils
+import MDReactComponent from 'markdown-react-js';
+import { Wrapper } from '../../utils/class';
+import { RED } from '../../utils/variables';
+import { rem } from '../../utils/helpers';
+
+export const Section = styled(Wrapper)`
+    width: 100%;
+    text-align: center;
+    background-size: cover;
+    color: #FFF;
+`;
