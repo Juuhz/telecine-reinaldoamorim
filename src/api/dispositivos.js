16f8aafd11481c9a8dbbe0298ef7d9001797885b
Reinaldo <juuhz.easy@gmail.com>
2019-05-01 23:57:50 -0300

    [Update] - Salvando processo de desenvolvimento

---
+++ src/api/dispositivos.js
@@ -0,0 +1,51 @@
+const DispositivosProps = {
+    "title": "Dispositivos",
+    "itens": [
+        {
+            "title": "O Melhor catálogo. Em todas as telas.",
+            "image": {
+                "src": "/static/images/1.jpg",
+                "alt": "Cena do filme Tomb Raider",
+                "title": "O Melhor catálogo. Em todas as telas."
+            },
+            "list": {
+                "title": "Assista na TV",
+                "itens": [
+                    {
+                        "label": "Smart Tvs"
+                    },
+                    {
+                        "label": "Chromecast"
+                    },
+                    {
+                        "label": "Xbox One"
+                    }
+                ]
+            }
+        },
+        {
+            "title": false,
+            "image": {
+                "src": "/static/images/2.jpg",
+                "alt": "Cena do filme: Forma da àgua",
+                "title": "Assista onde você quiser"
+            },
+            "list": {
+                "title": "Assista na TV",
+                "itens": [
+                    {
+                        "label": "Android"
+                    },
+                    {
+                        "label": "IOS"
+                    },
+                    {
+                        "label": "PC"
+                    }
+                ]
+            }
+        }
+    ]
+}
+
+export default DispositivosProps;
