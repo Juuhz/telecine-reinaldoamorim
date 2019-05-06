e6a75de0d33b1d8b578ccfb0b59c837e2fa075a0
Reinaldo <juuhz.easy@gmail.com>
2019-05-05 21:01:07 -0300

    [Feature] - Adicionando Jest

--- src/components/template/page.js
+++ src/components/template/page.js
@@ -8,7 +8,7 @@ const Page = ({ children, title = 'Assista filmes online onde você quiser pelo 
       <meta charSet='utf-8' />
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       <meta name='author' content='Reinaldo Amorim' />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
       <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, maximum-scale=1, user-scalable=no" />
       <link rel="icon" type="image/png" href="../../../static/images/favicon.ico" />
       <link rel="stylesheet" href="../../../static/css/style.css" />
